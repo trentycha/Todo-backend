5ddda8390c080c006f590e578328c962b7eb0631
trentycha <trentycharlene@gmail.com>
2025-09-09 21:40:53 +0200

    Modifications du Service

--- src/main/java/fr/lents/go/GreetingResource.java
+++ src/main/java/fr/lents/go/GreetingResource.java
@@ -1,5 +1,7 @@
 package fr.lents.go;
 
+import fr.lents.go.dto.TodoItemDto;
+import fr.lents.go.repository.TodoRepository;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -7,6 +9,14 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    private final TodoRepository todoRepository;
+
+    public GreetingResource(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
