df15fe96d7c10cb239cd6a0e0c1f5b3d51d03ec8
trentycha <trentycharlene@gmail.com>
2025-09-11 21:18:10 +0200

    Modifications du Controller

--- src/main/java/fr/lents/go/controller/TodoController.java
+++ src/main/java/fr/lents/go/controller/TodoController.java
@@ -1,4 +1,27 @@
 package fr.lents.go.controller;
 
+import fr.lents.go.dto.TodoItemDto;
+import fr.lents.go.service.TodoService;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+@Path("/todo")
 public class TodoController {
+
+    public final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TodoItemDto> getallTodo() {
+        return todoService.getAll();
+    }
 }
+
+
